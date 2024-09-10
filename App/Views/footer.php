48eab4383b3a6f3c5cd64d74f9c25eeb5c4cb817
Gabyrxv <ggoncalvesabati@gmail.com>
2024-09-10 14:23:35 -0300

    segundo

---
+++ App/Views/footer.php
@@ -0,0 +1,13 @@
+<footer class="bg-dark p-5 text-light">
+<div class="container text-center">
+<small> Aula de PHP Orientado a Objetos e MVC. Versão <?=
+APP_VERSAO ?>
+
+<div class="border-top mt-3">
+&COPY; 2024 - <?= date('V') ?> IFRO -
+Guajará-Mirim / Informática - Programação Web
+
+</div>
+</small>
+</div>
+</footer>
